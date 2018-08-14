e13b00569e1bd7786bb0d89d69cb11dea3d69f1d
Taylor Williams <taylor@thewilliams.net>
2018-08-14 03:37:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require './environment'
-
+require_relative '/app/models/pirate.rb'
+require_relative '/app/models/ship.rb'
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :new
+    end
 
+    post '/pirates' do
+      erb :show
+    end
   end
 end
